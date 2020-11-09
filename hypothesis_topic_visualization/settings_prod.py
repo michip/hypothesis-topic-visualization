852f947b23d13e1aab78fc52d3cca291d024fbfc
Michael Perk <y0079915@tu-braunschweig.de>
2020-11-10 00:50:01 +0100

    Added missing migrations

--- hypothesis_topic_visualization/settings_prod.py
+++ hypothesis_topic_visualization/settings_prod.py
@@ -16,7 +16,7 @@ DATABASES['default'].update(db_from_env)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = BASE_DIR / 'staticfiles'  #. os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
