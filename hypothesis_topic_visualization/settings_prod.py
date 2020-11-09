1748c57dbbe3f53328920b2b58abc5656af213f6
Michael Perk <y0079915@tu-braunschweig.de>
2020-11-09 17:48:03 +0100

    Heroku settings

---
+++ hypothesis_topic_visualization/settings_prod.py
@@ -0,0 +1,21 @@
+from .settings import *
+
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = BASE_DIR / 'staticfiles'  #. os.path.join(BASE_DIR, 'staticfiles')
+
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
