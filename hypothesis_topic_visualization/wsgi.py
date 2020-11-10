20ea83019566303bb80f68dd0078989748f20b9c
Michael Perk <y0079915@tu-braunschweig.de>
2020-11-10 01:08:59 +0100

    Static File Fixing

--- hypothesis_topic_visualization/wsgi.py
+++ hypothesis_topic_visualization/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hypothesis_topic_visualization.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hypothesis_topic_visualization.settings_prod')
 
 application = get_wsgi_application()
