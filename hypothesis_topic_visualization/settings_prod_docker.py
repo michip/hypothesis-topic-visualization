546367bed2030aaa7c1769ac14fdc6093e5cb997
Michael Perk <y0079915@tu-braunschweig.de>
2021-02-10 16:57:38 +0100

    Added configurations for docker-compose

---
+++ hypothesis_topic_visualization/settings_prod_docker.py
@@ -0,0 +1,15 @@
+from .settings import *
+import os
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
